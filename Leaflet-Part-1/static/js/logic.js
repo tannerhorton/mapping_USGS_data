2b83876a0cd7cb2ebe24ae37963f3f3f372e5610
Tanner Horton <Mtannerar13@gmail.com>
2023-02-02 14:19:45 -0500

    getting quake near LA to show up

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -16,8 +16,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
 // create object for the magnitude
-function circleSize(magnitude) {
-    return magnitude * 5
+function markerSize(magnitude) {
+    return magnitude * 6
   }
 
 // create object for the color 
@@ -27,139 +27,59 @@ function circleColor(depth) {
     else return "yellow"
 }
 
-// // Create an overlays object to add to the layer control.
-// var overlays = {
-//   "Coming Soon": layers.COMING_SOON,
-//   "Empty Stations": layers.EMPTY,
-//   "Low Stations": layers.LOW,
-//   "Healthy Stations": layers.NORMAL,
-//   "Out of Order": layers.OUT_OF_ORDER
-// };
+// read in earthquake data 
+d3.json(url).then(function(data) {
 
-// // Create a control for our layers, and add our overlays to it.
-// L.control.layers(null, overlays).addTo(map);
+    // use the geoJson function to draw map
+    L.geoJson(data, {
 
-// // Create a legend to display information about our map.
-// var info = L.control({
-//   position: "bottomright"
-// });
+        // passing in mapStyle for styling
+        pointToLayer: function (feature, latlng) {
 
-// // When the layer control is added, insert a div with the class of "legend".
-// info.onAdd = function() {
-//   var div = L.DomUtil.create("div", "legend");
-//   return div;
-// };
-// // Add the info legend to the map.
-// info.addTo(map);
+            // style each feature based on conditionals above
+            return new L.circleMarker(latlng, {
+                radius: 5,
+                color: circleColor(feature.geometry.coordinates[2]),
+                fillOpacity: 0.5,
+                weight: 1.2
+              });
+        },
 
-// // Initialize an object that contains icons for each layer group.
-// var icons = {
-//   COMING_SOON: L.ExtraMarkers.icon({
-//     icon: "ion-settings",
-//     iconColor: "white",
-//     markerColor: "yellow",
-//     shape: "star"
-//   }),
-//   EMPTY: L.ExtraMarkers.icon({
-//     icon: "ion-android-bicycle",
-//     iconColor: "white",
-//     markerColor: "red",
-//     shape: "circle"
-//   }),
-//   OUT_OF_ORDER: L.ExtraMarkers.icon({
-//     icon: "ion-minus-circled",
-//     iconColor: "white",
-//     markerColor: "blue-dark",
-//     shape: "penta"
-//   }),
-//   LOW: L.ExtraMarkers.icon({
-//     icon: "ion-android-bicycle",
-//     iconColor: "white",
-//     markerColor: "orange",
-//     shape: "circle"
-//   }),
-//   NORMAL: L.ExtraMarkers.icon({
-//     icon: "ion-android-bicycle",
-//     iconColor: "white",
-//     markerColor: "green",
-//     shape: "circle"
-//   })
-// };
+        // Give each feature a popup that describes the earthquake.
+        onEachFeature: function(feature,layer) {
+            // add popup for borough and neighborhood
+            layer.bindPopup(`<h2>${feature.properties.place}</h2><hr>
+            <h3>Magnitude: ${feature.properties.mag}, Depth: ${feature.geometry.coordinates[2]} </h3><hr>
+            <p>${new Date(feature.properties.time)}</p>`).addTo(map)
+        }
+    })
+});
 
-// // Perform an API call to the Citi Bike station information endpoint.
-// d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_information.json").then(function(infoRes) {
+// create legend
+var legend = L.control({ 
+    position: 'bottomright' 
+  });
 
-//   // When the first API call completes, perform another call to the Citi Bike station status endpoint.
-//   d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_status.json").then(function(statusRes) {
-//     var updatedAt = infoRes.last_updated;
-//     var stationStatus = statusRes.data.stations;
-//     var stationInfo = infoRes.data.stations;
+// function to add legend info
+legend.onAdd = function () {
 
-//     // Create an object to keep the number of markers in each layer.
-//     var stationCount = {
-//       COMING_SOON: 0,
-//       EMPTY: 0,
-//       LOW: 0,
-//       NORMAL: 0,
-//       OUT_OF_ORDER: 0
-//     };
+    var div = L.DomUtil.create('div', 'info legend');
 
-//     // Initialize stationStatusCode, which will be used as a key to access the appropriate layers, icons, and station count for the layer group.
-//     var stationStatusCode;
+    // create magnitude scale
+    var scale = [-10, 10, 30, 50, 70, 90];
 
-//     // Loop through the stations (they're the same size and have partially matching data).
-//     for (var i = 0; i < stationInfo.length; i++) {
+    // create corresponding color scale 
+    var colors = ['#74F906', '#D4F906','#F9D106', '#F9A406', '#F95B06', '#F91206'];
 
-//       // Create a new station object with properties of both station objects.
-//       var station = Object.assign({}, stationInfo[i], stationStatus[i]);
-//       // If a station is listed but not installed, it's coming soon.
-//       if (!station.is_installed) {
-//         stationStatusCode = "COMING_SOON";
-//       }
-//       // If a station has no available bikes, it's empty.
-//       else if (!station.num_bikes_available) {
-//         stationStatusCode = "EMPTY";
-//       }
-//       // If a station is installed but isn't renting, it's out of order.
-//       else if (station.is_installed && !station.is_renting) {
-//         stationStatusCode = "OUT_OF_ORDER";
-//       }
-//       // If a station has less than five bikes, it's status is low.
-//       else if (station.num_bikes_available < 5) {
-//         stationStatusCode = "LOW";
-//       }
-//       // Otherwise, the station is normal.
-//       else {
-//         stationStatusCode = "NORMAL";
-//       }
+    // for loop to match scale and colors 
+    for (var i = 0; i < scale.length; i++) {
 
-//       // Update the station count.
-//       stationCount[stationStatusCode]++;
-//       // Create a new marker with the appropriate icon and coordinates.
-//       var newMarker = L.marker([station.lat, station.lon], {
-//         icon: icons[stationStatusCode]
-//       });
+        div.innerHTML += "<i style='background: " + colors[i] + "'></i> "
+        + scale[i] + (scale[i + 1] ? "&ndash;" + scale[i + 1] + "<br>" : "+");
+    }
 
-//       // Add the new marker to the appropriate layer.
-//       newMarker.addTo(layers[stationStatusCode]);
+    return div;
+  };
 
-//       // Bind a popup to the marker that will  display on being clicked. This will be rendered as HTML.
-//       newMarker.bindPopup(station.name + "<br> Capacity: " + station.capacity + "<br>" + station.num_bikes_available + " Bikes Available");
-//     }
-
-//     // Call the updateLegend function, which will update the legend!
-//     updateLegend(updatedAt, stationCount);
-//   });
-// });
-
-// // Update the legend's innerHTML with the last updated time and station count.
-// function updateLegend(time, stationCount) {
-//   document.querySelector(".legend").innerHTML = [
-//     "<p>Updated: " + moment.unix(time).format("h:mm:ss A") + "</p>",
-//     "<p class='out-of-order'>Out of Order Stations: " + stationCount.OUT_OF_ORDER + "</p>",
-//     "<p class='coming-soon'>Stations Coming Soon: " + stationCount.COMING_SOON + "</p>",
-//     "<p class='empty'>Empty Stations: " + stationCount.EMPTY + "</p>",
-//     "<p class='low'>Low Stations: " + stationCount.LOW + "</p>",
-//     "<p class='healthy'>Healthy Stations: " + stationCount.NORMAL + "</p>"
-//   ].join("");
-// }
+//   add legend to map 
+  legend.addTo(map);
